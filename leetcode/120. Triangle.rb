c8e037066cc30e0eb65083c24266da2d5e1816d9
hany <hanysayed.a@gmail.com>
2022-03-27 02:31:59 +0200

    finish linked list

--- leetcode/120. Triangle.rb
+++ leetcode/120. Triangle.rb
@@ -4,13 +4,65 @@
 # @return {Integer}
 def minimum_total(triangle)
   sum = 0
-  triangle.each do |tr|
-    sum += tr.min
+  sum_next = 0
+  result = []
+  triangle.each_with_index do |tr, index|
+    if index == 0
+      sum += tr[index]
+      result.push(tr[index])
+    elsif index == triangle.length - 1
+      sum += tr.min
+      result.push(tr.min)
+    elsif index - 1 >= 0
+      selected = tr[index] > tr[index - 1] ? tr[index - 1] : tr[index]
+      sum += selected
+      result.push(selected)
+    end
+    # if index - 1 >= 0 && index != triangle.length - 1
+    #   selected = tr[index] > tr[index - 1] ? tr[index - 1] : tr[index]
+    #   sum += selected
+    #   result.push(selected)
+    # else
+    #   sum += tr.min
+    #   result.push(tr.min)
+    # end
+    # if index - 1 >= 0
+    #   selected = tr[index] > tr[index - 1] ? tr[index - 1] : tr[index]
+    #   sum += selected
+    #   result.push(selected)
+    # else
+    #   puts triangle.length
+    #   puts index
+    #   if index == triangle.length - 1
+    #     puts 'aaaaa'
+    #     sum += tr.min
+    #     result.push(tr.min)
+    #   else
+    #     sum += tr[index]
+    #     result.push(tr[index])
+    #   end
+    # end
+
+    # if tr[index]
+    #   sum += tr[index]
+    # elsif index - 1 >= 0
+    #   unless tr[index] > tr[index - 1]
+    #     sum-= tr[index]
+    #     sum+= tr[index - 1]
+    #   end
+    # end
   end
+  puts result.to_s
   sum
+
 end
 
 triangle = [[2], [3, 4], [6, 5, 7], [4, 1, 8, 3]]
-triangle = [[-1],[2,3],[1,-1,-3]]
+triangle = [[-1], [2, 3], [1, -1, -3]]
+# -1
 
 puts minimum_total(triangle)
+
+#    -1
+#   2   3
+# 1   -1   -3
--- leetcode/5. Longest Palindromic Substring.rb
+++ leetcode/5. Longest Palindromic Substring.rb
@@ -3,75 +3,58 @@
 # @param {String} s
 # @return {String}
 def longest_palindrome(s)
-  return s if s.length == 1
-  return s if s.length == 2 && s == s.reverse
-  result = ''
-  reverse_result = []
-  str_arr = s.chars
-  str_arr.each do |char|
-    result += char
-    reverse_result.push(result)
-  end
-  max_palin = ''
-  puts reverse_result.length
-  reverse_result.each do |ch|
-    if ch == ch.reverse
-      max_palin = ch
-    end
-    index = 1
-    while index < ch.length
-      y = ch[index..ch.length]
-      if y == y.reverse
-        max_palin = y.length > max_palin.length ? y : max_palin
-      end
-      index += 1
-    end
-  end
-  max_palin
-end
-
-def longest_palindrome2(s)
+  # cause time limit exceed but it is right
   return s if s.length == 1
   return s if s.length == 2 && s == s.reverse
   result = ''
   max_plain = ''
   start = 0
-  last = 0
   str_arr = s.chars
-  reverse_result = []
-
-  # "babad"
   while start < s.length
     result += str_arr[start]
     if result == result.reverse
-      reverse_result.push(result)
       max_plain = result.length > max_plain.length ? result : max_plain
     end
     index = 1
     while index < result.length
       y = result[index..result.length]
       if y == y.reverse
-        reverse_result.push(y)
         max_plain = y.length > max_plain.length ? y : max_plain
       end
       index += 1
-      # last = index
     end
-    # puts index
-    start+= 1
-    # result = str_arr[start..str_arr.length].join('')
-
+    start += 1
   end
-  puts result
-
-  puts reverse_result.to_s
   max_plain
 end
 
+def longest_palindrome2(s)
+  return "" if s.length < 1
+  max_sub_start = 0
+  max_sub_length = 0
+  (0..s.length - 1).each do |index|
+    max = [expand_around_center(s, index, index), expand_around_center(s, index, index + 1)].max
+    if max > max_sub_length
+      max_sub_length = max
+      temp = ((max_sub_length - 1) / 2).floor
+      max_sub_start = index - temp
+    end
+  end
+  s[max_sub_start..max_sub_length ].chars.take(max_sub_length).join
+end
+
+def expand_around_center(s, left, right)
+  while left >= 0 && right < s.length && s[left] == s[right]
+    left -= 1
+    right += 1
+  end
+  right - left - 1
+end
+
 # s = "babad"
-# s = "cbbd"
-s = "ac"
+s = "cbbd"
+# s = "ac"
 # s = "ccc"
-s = "eabcb"
+# s = "eabcb"
 
 puts longest_palindrome2(s)
